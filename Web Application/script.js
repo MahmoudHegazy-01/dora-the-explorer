b5aacdb0a76a6ad1dec2b8ca3760c3706d0b1be7
mohamedgamal6606 <chixoisthebest@gmail.com>
2024-04-25 14:05:30 +0200

    made Login and register redirect to respective pages

--- Web Application/script.js
+++ Web Application/script.js
@@ -1,7 +1,20 @@
-function validateForm() {
-  let x = document.forms["login"]["username"].value;
-  if (x == "Admin") {
-    alert("Name must be filled out");
-    return true;
+function login() {
+  var x = document.getElementById("username").value;
+  var y = document.getElementById("password").value;
+  if ((x === "admin") & (y === "admin")) {
+    window.location.href = "Admin/admin.html";
   }
+  if ((x === "Donor") & (y === "donor")) {
+    window.location.href = "Donor/donor.html";
+  }
+  if ((x === "Organization") & (y === "organization")) {
+    window.location.href = "Organization/organization.html";
+  }
+  if ((x === "User") & (y === "user")) {
+    window.location.href = "User/user.html";
+  }
+}
+
+function register() {
+  window.location.href = "register.html";
 }
