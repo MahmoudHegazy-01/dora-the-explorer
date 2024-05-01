941df3ea833d5f496ca5764a220dc1a99457e991
Youssef Sherif Sabry <youssefazer26112002@gmail.com>
2024-05-01 17:45:06 +0300

    added admin java script file with dummy data & empty functions

---
+++ Web Application/Admin/adminScript.js
@@ -0,0 +1,50 @@
+const orgList0 = new Array("Hope","0123450","hope@gmail.com");
+const orgList1 = new Array("Light","0123451","light@gmail.com");
+const orgList2 = new Array("Future","0123452","future@gmail.com");
+
+const donList0 = new Array("Ahmed Mohamed","0123460","ahmed@gmail.com");
+const donList1 = new Array("Ali Mohamed","0123461","ali@gmail.com");
+const donList2 = new Array("Karim Mohamed","0123462","karim@gmail.com");
+
+const orgSubmissions0 = new Array(orgList1, "Document1", "Document2");
+const orgSubmissions1 = new Array(orgList2, "Document1", "Document2");
+
+const donSubmissions0 = new Array(donList1, "Document1", "Document2");
+const donSubmissions1 = new Array(donList2, "Document1", "Document2");
+
+
+const orgList = {orgList0,orgList1,orgList2};
+const donList = {donList0,donList1,donList2};
+const orgSubmissions = {orgSubmissions0,orgSubmissions1};
+const donSubmissions = {donSubmissions0,donSubmissions1};
+const orgRequests = {orgList1,orgList2};
+const donRequests = {donList1,donList2};
+const regOrg = {orgList0};
+
+function orgListDisplay() {
+    
+}
+
+function donListDisplay() {
+    
+}
+
+function orgSubmissionsDisplay() {
+    
+}
+
+function donSubmissionsDisplay() {
+    
+}
+
+function orgRequestsDisplay() {
+    
+}
+
+function donRequestsDisplay() {
+    
+}
+
+function regOrgDisplay() {
+    
+}
