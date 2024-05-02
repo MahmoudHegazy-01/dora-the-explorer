225ec6b4a2cd8e313f0bdcaa620cdefab6a920ae
Youssef Sherif Sabry <youssefazer26112002@gmail.com>
2024-05-02 18:01:34 +0300

    modified admin tabs and added empty htmls

--- Web Application/Admin/adminScript.js
+++ Web Application/Admin/adminScript.js
@@ -48,3 +48,42 @@ function donRequestsDisplay() {
 function regOrgDisplay() {
     
 }
+
+function handleClick1(event) {
+    // Prevent the default behavior of the link
+    event.preventDefault();
+
+    // Create ul element
+    var ul = document.createElement('ul');
+    ul.className = "dropdown-menu position-absolute d-grid gap-1 p-2 rounded-3 mx-0 shadow w-220px";
+    ul.setAttribute('data-bs-theme', 'light');
+
+    // Calculate the position of the button
+    var buttonRect = event.target.getBoundingClientRect();
+    var top = buttonRect.top + buttonRect.height;
+    var left = buttonRect.left;
+
+    // Set the position of the dropdown
+    ul.style.top = top + 'px';
+    ul.style.left = left + 'px';
+
+    // Define the URLs for the dropdown items
+    var urls = ["orglist.html", "donlist.html"]; // Example URLs
+
+    // Create li elements and anchor elements
+    var actions = ["OrganizationsList", "DonorsList"];
+    actions.forEach(function(actionText, index) {
+        var li = document.createElement('li');
+        var a = document.createElement('a');
+        a.className = "dropdown-item rounded-2";
+        a.href = urls[index]; // Set the href attribute to the corresponding URL
+        a.textContent = actionText;
+        li.appendChild(a);
+        ul.appendChild(li);
+    });
+
+    // Append the ul element to the document body
+    document.body.appendChild(ul);
+}
+
+
