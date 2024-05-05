31dcf3806a80e3840163912c95b944869d286187
HazemEssam13 <161717863+HazemEssam13@users.noreply.github.com>
2024-05-06 01:15:29 +0300

    Added a page where the doctor can upload his/her qualifications documents

---
+++ Web Application/Donor/donorrole.js
@@ -0,0 +1,9 @@
+function selectdoctor(){
+    window.location.href = "/Web Application/Donor/donordoctor.html";
+}
+function selectteacher(){
+    window.location.href = "/Web Application/Donor/donorteacher.html";
+}
+function selectteacher(){
+    window.location.href = "/Web Application/Donor/donorteacher.html";
+}
